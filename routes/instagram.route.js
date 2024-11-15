ebf9277c134cb72bfb74f750022dfdf62c6fd098
Ravi <developerravi8@gmail.com>
2024-11-15 18:53:30 +0530

    waah

--- routes/instagram.route.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const {getInstagramUserProfileData} = require('../controller/instagram.controller.js');
-
-const router = Router();
-
-
-router.get('/getUserProfileData/:InstagramId', getInstagramUserProfileData)
-
-module.exports = router
